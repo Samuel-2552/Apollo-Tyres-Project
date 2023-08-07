8e4de9443fbde5bfebaf57008ed644c83eca5759
Samuel <201501502@rajalakshmi.edu.in>
2023-08-07 10:50:19 +0530

    updated file tab

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import subprocess
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, QVBoxLayout, QWidget, QSizePolicy
@@ -34,9 +35,24 @@ class BrowserWindow(QMainWindow):
         # Create the menu bar
         menubar = self.menuBar()
         file_menu = menubar.addMenu("File")
-        exit_action = QAction("Exit", self)
+
+       # Open Folder action
+        open_folder_action = QAction(QIcon("icon_folder.png"), "Check Reports", self)
+        open_folder_action.triggered.connect(self.open_folder)
+        file_menu.addAction(open_folder_action)
+
+        # Restart action
+        restart_action = QAction(QIcon("icon_restart.png"), "Restart", self)
+        restart_action.triggered.connect(self.restart_application)
+        file_menu.addAction(restart_action)
+
+        # Exit action
+        exit_action = QAction(QIcon("icon_exit.png"), "Exit", self)
         exit_action.triggered.connect(self.close)
         file_menu.addAction(exit_action)
+        
+
+        
 
         # Load Google by default
         self.load_url("http://127.0.0.1:5000")
@@ -50,6 +66,23 @@ class BrowserWindow(QMainWindow):
 
     def update_address_bar(self, url):
         pass
+
+    def restart_application(self):
+        # Get the current executable and arguments
+        executable = sys.executable
+        args = sys.argv
+
+        # Start a new instance of the application
+        subprocess.Popen([executable] + args)
+        self.close()
+
+    def open_folder(self):
+        initial_dir = os.getcwd()  # Get the current working directory where the application runs
+        initial_dir += "/Reports"
+        os.startfile(initial_dir)
+        # folder = QFileDialog.getExistingDirectory(self, "Select Folder", initial_dir)
+        # if folder:
+        #     QDesktopServices.openUrl(QUrl.fromLocalFile(folder))
 
 if __name__ == "__main__":
     # Run the Flask app in a separate process

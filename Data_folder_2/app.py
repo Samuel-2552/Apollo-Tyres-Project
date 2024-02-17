f25e95dd74250b3caa2c9b43734a560a279e4aad
Sivabalan10 <135865311+Sivabalan10@users.noreply.github.com>
2024-02-17 14:46:58 +0530

    // Updated database

--- Data_folder_2/app.py
+++ Data_folder_2/app.py
@@ -29,9 +29,9 @@ def create_database_and_tables():
     if not users_table_exists:
         cursor.execute('''CREATE TABLE users (
                             id INTEGER PRIMARY KEY,
-                            username TEXT,
+                            username TEXT UNIQUE,
                             password TEXT,
-                            uuid INTEGER DEFAULT 0,
+                            uuid TEXT DEFAULT 0,
                             initial_state INTEGER DEFAULT 0
                         )''')
 
@@ -59,6 +59,7 @@ def get_system_id():
         # System UUID (Universally Unique Identifier)
         for system in c.Win32_ComputerSystemProduct():
             system_info = system.UUID
+            print(system_info)
 
     except Exception as e:
         print(f"Error: {e}")
@@ -82,19 +83,19 @@ def update_uuid(username, new_uuid):
     print(f"UUID for user '{username}' updated successfully.")
 
 
-def update_initial_state(username, new_state):
+def update_initial_state(uuid, new_state):
     # Connect to SQLite database
     conn = sqlite3.connect('config.db')
     cursor = conn.cursor()
 
     # Update initial state for the given username
-    cursor.execute('''UPDATE users SET initial_state = ? WHERE username = ?''', (new_state, username))
+    cursor.execute('''UPDATE users SET initial_state = ? WHERE uuid = ?''', (new_state, uuid))
 
     # Commit changes and close connection
     conn.commit()
     conn.close()
 
-    print(f"Initial state for user '{username}' updated successfully.")
+    print(f"Initial state for user '{uuid}' updated successfully.")
 
 
 def create_new_user(username, password):
@@ -112,12 +113,14 @@ def create_new_user(username, password):
 
     print(f"New user '{username}' created successfully.")
 
+
 def get_db_connection():
     conn = sqlite3.connect('config.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-def login_post(user,password_1):
+
+def login_post(user, password_1):
     username = user
     password = password_1
 
@@ -132,11 +135,13 @@ def login_post(user,password_1):
     if user:
         if user['password'] == password:
             # Passwords match, redirect to the next page
-            return redirect(url_for('next_page'))
+            print("Password correct")
+            return True
         else:
-            return 'Invalid password'
+            return False
     else:
         print("Something went wrong while login!")
+
 
 def is_pingable(ip_address):
     """
@@ -173,21 +178,24 @@ def retrieve_users_data():
 
     return users_data
 
-def store_camera_ips(ip1,ip2,ip3,ip4, plcip, plcport):
+
+def store_camera_ips(ip1, ip2, ip3, ip4, plcip, plcport):
     # Connect to SQLite database
     conn = sqlite3.connect('config.db')
     cursor = conn.cursor()
 
     # Insert IP addresses into the cameras table
     cursor.execute('''INSERT INTO cameras (ip1, ip2, ip3, ip4, plcip, plcport)
-                      VALUES (?, ?, ?, ?, ?, ?)''', (ip1,ip2,ip3,ip4, plcip, plcport))
+                      VALUES (?, ?, ?, ?, ?, ?)''', (ip1, ip2, ip3, ip4, plcip, plcport))
 
     # Commit changes and close connection
     conn.commit()
     conn.close()
 
-def check_uuid_and_initial_state(uuid):
+
+def check_uuid_and_initial_state(uuid, username):
     # Connect to SQLite database
+    print("Checking uuid.........")
     conn = sqlite3.connect('config.db')
     cursor = conn.cursor()
 
@@ -200,28 +208,32 @@ def check_uuid_and_initial_state(uuid):
 
     if user_data is None:
         # If UUID not found, add it to the table with initial_state as 0
-        add_uuid_to_users(uuid)
-        return render_template("load.html")  # Redirect to load.html
+        add_uuid_to_users(uuid, username)
+        return "Load"
+        # return render_template("load.html")  # Redirect to load.html
     else:
         _, initial_state = user_data
         if initial_state == 1:
-            return render_template("live_feed.html")  # Redirect to live_feed.html
+            return "Live"
+            # return render_template("live_feed.html")  # Redirect to live_feed.html
         else:
-            return render_template("load.html")  # Redirect to load.html
+            return "Load"  # Redirect to load.html
 
-def add_uuid_to_users(uuid):
+
+
+def add_uuid_to_users(uuid, username):
     # Connect to SQLite database
+    print("added uuid")
     conn = sqlite3.connect('config.db')
     cursor = conn.cursor()
-
-    # Insert UUID into users table with initial_state as 0
-    cursor.execute('''INSERT INTO users (uuid, initial_state) VALUES (?, 0)''', (uuid,))
+    # Update the row corresponding to the username with the UUID
+    cursor.execute('''UPDATE users SET uuid = ?, initial_state = 0 WHERE username = ?''', (uuid, username))
 
     # Commit changes and close connection
     conn.commit()
     conn.close()
 
-    print(f"UUID '{uuid}' added to users table with initial state 0.")
+    print(f"UUID '{uuid}' added to users table for username '{username}' with initial state 0.")
 
 
 # --------------------------------------------------------------------------------------------
@@ -255,14 +267,31 @@ def load():
     username = request.form.get('username')
     password = request.form.get('password')
     # login
-    login_post(username,password)
+    val_1 = login_post(username, password)
+    if val_1:
+        print("valid")
+        current_uuid = get_system_id()
+        val = check_uuid_and_initial_state(current_uuid, username)
+        if val == "Load":
+            return render_template("load.html")
+        elif val == "Live":
+            return render_template("live_feed.html")
+    else:
+        print("invalid")
 
+#should not login
+    return render_template("load.html")
 
 
 @app.route('/next_page')
 def next_page():
     current_uuid = get_system_id()
-    check_uuid_and_initial_state(current_uuid)
+    val = check_uuid_and_initial_state(current_uuid)
+    if val == "Load":
+        return render_template("load.html")
+    elif val == "Live":
+        return render_template("live_feed.html")
+
 
 @app.route('/signup_page')
 def signup_page():
@@ -308,6 +337,20 @@ def testing():
     return content
 
 
+@app.route("/pinging", methods=['POST'])
+def pinging():
+    plc_ip = request.form.get('data_1')
+    res = is_pingable(plc_ip)
+    if res:
+        with open("static/js/db.txt", 'w') as file:
+            file.write('1\n')
+    else:
+        with open("static/js/db.txt", 'w') as file:
+            file.write('0\n')
+    with open("static/js/db.txt", 'r') as file:
+        content = file.read().strip()
+
+    return content
 def redirect_template():
     return render_template('live_feed.html')
 
@@ -320,20 +363,22 @@ def live_feed():
     ip2 = addresses[1]
     ip3 = addresses[2]
     ip4 = addresses[3]
-    plcip = "172.17"
-    plcport = 5000
+    plcip = data.get('plc_ip')
+    plcport = data.get('plc_port')
     store_camera_ips(ip1, ip2, ip3, ip4, plcip, plcport)
-    print("Ip stored")
-    redirect_template()
+    print("Ip and plc are stored...........")
+    sys_uuid = get_system_id()
+    update_initial_state(sys_uuid, 1)
     return jsonify({'success': True})
+
+@app.route('/live_feed_page')
+def live_feed_page():
+    return render_template("live_feed.html")
 
 
 @app.route('/live')
 def live():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-
-
 
 
 if __name__ == "__main__":
--- Data_folder_2/static/js/script.js
+++ Data_folder_2/static/js/script.js
@@ -8,6 +8,37 @@ function validate_frame(address) {
     // Prevent the default form submission behavior
     return false;
 }
+function check_ping(){
+    event.preventDefault();
+    let plc_ip = document.getElementById('plc_ip');
+    send_data_plc(plc_ip.value);
+}
+function send_data_plc(plcip){
+    $.ajax({
+        url: '/pinging',
+        type: 'POST',
+        data: { 'data_1': plcip },
+        success: function(response) {
+            // Create a paragraph element for displaying the result
+            let ch = document.getElementById('res_5');
+            // Change the paragraph content based on the response value
+            if (response.trim() === '0') {
+                ch.style.color = "red";
+                ch.innerHTML = "Failed!";
+            } else {
+            ch.style.color = "green";
+                ch.innerHTML = "Success";
+                tested+=1;
+            }
+            // Append the paragraph element below the corresponding input element
+
+        },
+        error: function(error) {
+            console.log(error);
+        }
+    });
+}
+
 let tested = 0;
 function sendData1(inputData, inputId,address_ret) {
     $.ajax({
@@ -42,23 +73,28 @@ function sendData1(inputData, inputId,address_ret) {
 function testing(){
   if(tested >= 4){
     var addresses = [];
-            for (var i = 1; i <= 4; i++) {
-                var address = document.getElementById("address_" + i).value;
-                addresses.push(address);
+        for (var i = 1; i <= 4; i++) {
+            var address = document.getElementById("address_" + i).value;
+            addresses.push(address);
+        }
+
+        var plc_ip = document.getElementById("plc_ip").value;
+        var plc_port = document.getElementById("plc_port").value;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", "/live_feed", true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                console.log(xhr.responseText);
+                alert(01);
+                return window.location.href = '/live_feed_page';
             }
+        };
+        var data = JSON.stringify({ addresses: addresses, plc_ip: plc_ip, plc_port: plc_port });
+        xhr.send(data);
 
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "/live_feed", true);
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 4 && xhr.status === 200) {
-                    console.log(xhr.responseText);
-                }
-            };
-            var data = JSON.stringify({ addresses: addresses });
-            xhr.send(data);
-
-            return window.location.href = '/live_feed'; // Prevent form submission
+         //// Prevent form submission
 
         }
 
